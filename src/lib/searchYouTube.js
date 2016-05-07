798abbe5966bcc7378caf03a2af230c0276697ac
tjcarskadon <tjcarskadon@gmail.com>
2016-05-07 16:27:19 -0700

    break the index but succeed in getting data from youtube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
   // TODO
+// console.log("Options: ", options);
+  var APIURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${options.key}&q=${options.query}&maxResults=${options.max}`;
+  // var APIURL = `https://www.youtube.com/embed/?key=${options.key}&q=${options.query}&maxResults=${options.max}`;
+
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: APIURL,
+    type: 'GET',
+    // data: null,
+    success: function(data) {
+      callback(data);
+    },
+    error: function(data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
